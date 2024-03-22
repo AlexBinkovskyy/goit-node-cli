5b7d900e13dec24913b36ba2b0ced97e6cb9937b
AlexBinkovskyy <alex_bin@ukr.net>
2024-03-22 14:45:15 +0200

    done

--- index.js
+++ index.js
@@ -1,37 +1,36 @@
 import { program } from "commander";
+import { listContacts, getContactById, removeContact, addContact } from "./contacts.js";
+
 program
-  .option("-a, --action <type>", "choose action")
-  .option("-i, --id <type>", "user id")
-  .option("-n, --name <type>", "user name")
-  .option("-e, --email <type>", "user email")
-  .option("-p, --phone <type>", "user phone");
-
+    .option("-a, --action <type>", "choose action")
+    .option("-i, --id <type>", "user id")
+    .option("-n, --name <type>", "user name")
+    .option("-e, --email <type>", "user email")
+    .option("-p, --phone <type>", "user phone");
 program.parse();
-
 const options = program.opts();
 
-// TODO: рефакторити
 async function invokeAction({ action, id, name, email, phone }) {
-  switch (action) {
-    case "list":
-      // ...
-      break;
+    switch (action) {
+        case "list":
+            console.table(await listContacts());
+            break;
 
-    case "get":
-      // ... id
-      break;
+        case "get":
+            console.table(await getContactById(id));
+            break;
 
-    case "add":
-      // ... name email phone
-      break;
+        case "add":
+            console.table(await addContact(name, email, phone));
+            break;
 
-    case "remove":
-      // ... id
-      break;
+        case "remove":
+            console.table(await removeContact(id));
+            break;
 
-    default:
-      console.warn("\x1B[31m Unknown action type!");
-  }
+        default:
+            console.warn("\x1B[31m Unknown action type!");
+    }
 }
 
 invokeAction(options);
